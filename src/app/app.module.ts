ac919cc6058c04ee4aabc088f8fca8b3683fc67f
Joe Monaco <jmonaco@hybirdhealth.com>
2019-11-08 15:22:22 -0500

    form works and creates valid json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { JsonFormComponent } from './components/json-form/json-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { JsonFormComponent } from "./components/json-form/json-form.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { HttpClient } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    JsonFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, JsonFormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/json-form/json-form.component.ts
+++ src/app/components/json-form/json-form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import {
+  FormBuilder,
+  FormArray,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-json-form",
@@ -7,25 +14,127 @@ import { FormBuilder } from "@angular/forms";
   styleUrls: ["./json-form.component.css"]
 })
 export class JsonFormComponent implements OnInit {
-  numOfQuestions = 0;
-  numQuestionsArr = [];
+  quizForm: FormGroup;
+  randomizeQuiz = false;
+  finalJSON = [{}];
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.quizForm = this.fb.group({
+      questions: this.fb.array([this.initQuestions()])
+    });
+  }
 
-  numberQuestions(value) {
-    this.numQuestionsArr = [];
-    for (let i = 0; i < value; i++) {
-      this.numQuestionsArr.push(null);
+  initQuestions() {
+    return this.fb.group({
+      //  ---------------------forms fields on x level ------------------------
+      Type: ["Type", [Validators.required]],
+      Text: ["Text", [Validators.required]],
+      Points: [, [Validators.required]],
+      // ---------------------------------------------------------------------
+      Answers: this.fb.array([this.initAnswers()]),
+      CorrectAnswers: this.fb.array([this.initCorretAnswers()]),
+      Randomize: [false]
+    });
+  }
+
+  initAnswers() {
+    return this.fb.group({
+      Answer: ["", [Validators.required]]
+    });
+  }
+
+  initCorretAnswers() {
+    return this.fb.group({
+      Correct: ["", [Validators.required]]
+    });
+  }
+
+  addQuestion() {
+    const control = <FormArray>this.quizForm.controls["questions"];
+    control.push(this.initQuestions());
+  }
+
+  deleteQuestion(index: number) {
+    const control = <FormArray>this.quizForm.controls["questions"];
+    if (control.length > 1) {
+      control.removeAt(index);
     }
   }
 
-  //Use blur for each input and then add to the array
-
-  update(index, value) {
-    console.log(index, value);
+  addAnswer(ix) {
+    const control = (<FormArray>this.quizForm.controls["questions"])
+      .at(ix)
+      .get("Answers") as FormArray;
+    control.push(this.initAnswers());
   }
 
-  submitQuiz() {}
+  deleteAnswer(index: number) {
+    const control = (<FormArray>this.quizForm.controls["questions"])
+      .at(index)
+      .get("Answers") as FormArray;
+    if (control.length > 1) {
+      control.removeAt(index);
+    }
+  }
+
+  addCorrect(ix) {
+    const control = (<FormArray>this.quizForm.controls["questions"])
+      .at(ix)
+      .get("CorrectAnswers") as FormArray;
+    control.push(this.initCorretAnswers());
+  }
+
+  deleteCorrect(index) {
+    const control = (<FormArray>this.quizForm.controls["questions"])
+      .at(index)
+      .get("CorrectAnswers") as FormArray;
+    if (control.length > 1) {
+      control.removeAt(index);
+    }
+  }
+
+  populateForm() {}
+
+  submit() {
+    let questions = this.quizForm.value.questions;
+    this.finalJSON[0]["num_questions"] = questions.length;
+    this.finalJSON[0]["randomize"] = this.randomizeQuiz;
+
+    let allQuestions = [];
+
+    for (let i = 0; i < questions.length; i++) {
+      let answers = [];
+      let correctAnswers = [];
+      for (let j = 0; j < questions[i]["Answers"].length; j++) {
+        answers.push(questions[i]["Answers"][j]["Answer"]);
+      }
+      for (let j = 0; j < questions[i]["CorrectAnswers"].length; j++) {
+        correctAnswers.push(questions[i]["CorrectAnswers"][j]["Correct"]);
+      }
+
+      allQuestions.push({
+        type: questions[i]["Type"],
+        text: questions[i]["Text"],
+        point_value: questions[i]["Points"],
+        answers: answers,
+        correct_answers: correctAnswers,
+        randomize: questions[i]["Randomize"]
+      });
+    }
+    this.finalJSON[0]["questions"] = allQuestions;
+    console.log(this.finalJSON);
+    this.saveJSON(JSON.stringify(this.finalJSON), "stuff.json");
+  }
+
+  saveJSON(text, filename) {
+    var a = document.createElement("a");
+    a.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-u," + encodeURIComponent(text)
+    );
+    a.setAttribute("download", filename);
+    a.click();
+  }
 }
